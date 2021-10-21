31b56316a03c5635d36a61e0c0f1e13c0313049f
yeahbutstill <mayadani28@gmail.com>
2021-10-21 21:19:05 +0700

    Start java validation

---
+++ src/test/java/com/yeahbutstill/javavalidation/JavaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.javavalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
